023a8d5b3e9e8382cdcc422e4c5c1c2f8e0238b4
AmiraAdel28 <amira.adel282001@gmail.com>
2025-02-07 23:12:32 +0200

    Commet1

---
+++ Examination_system/QuestionBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examination_system
+{
+    internal class QuestionBase
+    {
+
+
+        #region Properties
+        public string HeaderOfTheQuestion { get; set; }
+        public string BodyOfTheQuestion { get; set; }
+        public int Mark { get; set; }
+        public Answers AnswerList { get; set; }
+        public int[] RightAnswer { get; set; }
+
+
+        #endregion
+
+
+        #region Constructor
+        public QuestionBase(string header, string body, int mark, Answers answerList, int[] rightAnswer)
+        {
+            HeaderOfTheQuestion = header;
+            BodyOfTheQuestion = body;
+            Mark = mark;
+            AnswerList = answerList;
+            RightAnswer = rightAnswer;
+        }
+        #endregion
+
+    }
+}
